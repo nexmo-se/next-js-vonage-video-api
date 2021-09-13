57f5c4fab3919848b47a38ebd657ad8a354715b6
enricop89 <enrico.portolan@gmail.com>
2021-09-13 10:51:03 +0100

    - first commit

---
+++ components/Opentok.js
@@ -0,0 +1,101 @@
+import OT from '@opentok/client';
+import { useEffect } from 'react';
+
+const apiKey = '';
+const sessionId = '';
+const token = '';
+
+export default function Opentok() {
+  /* OT.initPublisher('', (err) => {
+    if (err) {
+      console.log('err', err);
+    }
+    console.log('Publisher created');
+  }); */
+
+  function handleError(error) {
+    if (error) {
+      console.error(error);
+    }
+  }
+
+  function initializeSession() {
+    const session = OT.initSession(apiKey, sessionId);
+
+    // Subscribe to a newly created stream
+    session.on('streamCreated', function streamCreated(event) {
+      const subscriberOptions = {
+        insertMode: 'append',
+        width: '100%',
+        height: '100%'
+      };
+      session.subscribe(
+        event.stream,
+        'subscriber',
+        subscriberOptions,
+        handleError
+      );
+    });
+
+    session.on('sessionDisconnected', function sessionDisconnected(event) {
+      console.log('You were disconnected from the session.', event.reason);
+    });
+
+    // initialize the publisher
+    const publisherOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    };
+    const publisher = OT.initPublisher(
+      'publisher',
+      publisherOptions,
+      handleError
+    );
+
+    // Connect to the session
+    session.connect(token, function callback(error) {
+      if (error) {
+        handleError(error);
+      } else {
+        // If the connection is successful, publish the publisher to the session
+        session.publish(publisher, handleError);
+      }
+    });
+  }
+
+  useEffect(() => {
+    initializeSession();
+  }, []);
+
+  /* render() {
+    const { className, style } = this.props;
+    return <div className={className} style={style} ref={(node) => { this.node = node; }} />;
+  } */
+  return (
+    <div id="videos">
+      <div id="publisher"></div>
+      <div id="subscriber"></div>
+      <style jsx>{`
+        #videos {
+          position: relative;
+          width: 100%;
+          height: 100%;
+          margin-left: auto;
+          margin-right: auto;
+          display: flex;
+        }
+
+        #subscriber {
+          width: 360px;
+          height: 240px;
+        }
+
+        #publisher {
+          width: 360px;
+          height: 240px;
+        }
+      `}</style>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+const OpentokComponent = dynamic(() => import('./../components/Opentok'), {
+  ssr: false
+});
 
 export default function Home() {
   return (
@@ -9,55 +13,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className="title">Welcome to Next.js with Vonage Video API!</h1>
+        <OpentokComponent></OpentokComponent>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -74,25 +32,6 @@ export default function Home() {
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
           justify-content: center;
           align-items: center;
         }
@@ -205,5 +144,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
